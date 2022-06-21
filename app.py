1a036c4b144e9a74b79fa895b60f0580e090d95c
Prajjwal Arora <prajjwal.arora@anduriltechnologies.com>
2022-06-21 07:02:45 +0530

    cord added

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
 # app.py
 import re
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,make_response
 # from face import face_recog
+from flask_cors import CORS
 from audio import audio_recog
 import os
 app = Flask(__name__)
-
-@app.route('/auth/face-verify/', methods=['POST'])
+CORS(app)
+@app.route('/auth/face-verify/', methods=['POST', "OPTIONS"])
 def face_verify():
-    reqBody = request.get_json();
+    if request.method == "OPTIONS": 
+        return _build_cors_preflight_response()
+    else:
+        reqBody = request.get_json()
     # userImg =reqBody["userImg"]
     # verifyImg =reqBody["verifyImg"]
     # result = face_recog(userImg,verifyImg)
     # Return the response in json format
-    return jsonify(reqBody)
+        return _corsify_actual_response(jsonify(reqBody))
 
-@app.route('/auth/audio-verify/', methods=['POST'])
+@app.route('/auth/audio-verify/', methods=['POST',"OPTIONS"])
 def audio_verify():
-    
-    reqBody = request.get_json();
-    userAudio =reqBody["userAudio"]
-    result = audio_recog(userAudio)
-    
-    #print(message)
-    #response = {}
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    else:
+        reqBody = request.get_json();
+        userAudio =reqBody["userAudio"]
+        result = audio_recog(userAudio)
+        return _corsify_actual_response(jsonify(result))
 
-    #response["isAudioVerified"] = True
-    # Return the response in json format
-    return jsonify(result)
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 
 @app.route('/')
 def index():
